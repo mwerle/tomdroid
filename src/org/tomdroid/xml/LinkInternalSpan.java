83128a8da338646c4a4cc7a71d403f2ccdc19d6a
Michael Werle <micha@michaelwerle.com>
2025-03-10 17:49:53 +0900

    feat: prevent note title links inside words
    Fixes #201 (lp1234279)
    
    This change modifies the match filter to also filter out matches which
    are preceded or followed by a unicode word or digit character.
    
    This allows, for example, "bar" to be linked in something like "foo/bar",
    but not inside "foobar".

--- src/org/tomdroid/xml/LinkInternalSpan.java
+++ src/org/tomdroid/xml/LinkInternalSpan.java
@@ -2,21 +2,21 @@
  * Tomdroid
  * Tomboy on Android
  * http://www.launchpad.net/tomdroid
- * 
+ *
  * Copyright 2012 Koichi Akabe <vbkaisetsu@gmail.com>
- * 
+ *
  * This file is part of Tomdroid.
- * 
+ *
  * Tomdroid is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Tomdroid is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Tomdroid.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -40,46 +40,68 @@ import android.view.View;
  */
 public class LinkInternalSpan extends ClickableSpan {
 
-	// Logging info
-	private static final String TAG = "LinkInternalSpan";
-	
-	private String title;
-	public LinkInternalSpan(String title) {
-		super();
-		this.title = title;
-	}
+    // Logging info
+    private static final String TAG = "LinkInternalSpan";
 
-	@Override
-	public void onClick(View v) {
-		Activity act = (Activity)v.getContext();
-		int id = NoteManager.getNoteId(act, title);
-		Uri intentUri;
-		if(id != 0) {
-			intentUri = Uri.parse(Tomdroid.CONTENT_URI.toString()+"/"+id);
-		} else {
-			/* TODO: open new note */
-			TLog.d(TAG, "link: {0} was clicked", title);
-			return;
-		}
-		Intent i = new Intent(Intent.ACTION_VIEW, intentUri);
-		act.startActivity(i);
-	}
-	
-	public static MatchFilter getNoteLinkMatchFilter(final SpannableStringBuilder noteContent, final LinkInternalSpan[] links) {
-		
-		return new MatchFilter() {
-			
-			public boolean acceptMatch(CharSequence s, int start, int end) {
-				int spanstart, spanend;
-				for(LinkInternalSpan link: links) {
-					spanstart = noteContent.getSpanStart(link);
-					spanend = noteContent.getSpanEnd(link);
-					if(!(end <= spanstart || spanend <= start)) {
-						return false;
-					}
-				}
-				return true;
-			}
-		};
-	}
+    private String title;
+
+    public LinkInternalSpan(String title) {
+        super();
+        this.title = title;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Activity act = (Activity) v.getContext();
+        int id = NoteManager.getNoteId(act, title);
+        Uri intentUri;
+        if (id != 0) {
+            intentUri = Uri.parse(Tomdroid.CONTENT_URI.toString() + "/" + id);
+        } else {
+            /* TODO: open new note */
+            TLog.d(TAG, "link: {0} was clicked", title);
+            return;
+        }
+        Intent i = new Intent(Intent.ACTION_VIEW, intentUri);
+        act.startActivity(i);
+    }
+
+
+    /** Create a match filter when creating links using Linkify.
+     *
+     * This match filter filters out the following conditions:
+     * - any span which is already a link
+     * - matches which are inside a word
+     *
+     * @param noteContent The note content
+     * @param links The links to check against
+     * @return The match filter
+     */
+    public static MatchFilter getNoteLinkMatchFilter(
+            final SpannableStringBuilder noteContent,
+            final LinkInternalSpan[] links) {
+
+        return new MatchFilter() {
+
+            // Accept a match only if it is not already a link and if it is not
+            // inside a word.
+            public boolean acceptMatch(CharSequence s, int start, int end) {
+                int spanstart, spanend;
+                for (LinkInternalSpan link : links) {
+                    spanstart = noteContent.getSpanStart(link);
+                    spanend = noteContent.getSpanEnd(link);
+                    if (!(end <= spanstart || spanend <= start)) {
+                        return false;
+                    }
+                }
+                if (start > 0 && Character.isLetterOrDigit(s.charAt(start - 1))) {
+                    return false;
+                }
+                if (end < s.length() - 1 && Character.isLetterOrDigit(s.charAt(end))) {
+                    return false;
+                }
+                return true;
+            }
+        };
+    }
 }
